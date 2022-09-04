698480650407779849011269ea7dc79861033c8c
headbwoi <tochukwujohn24@gmail.com>
2022-09-04 22:03:51 +0100

    moved why variants to custom variant file in variant folder

--- pages/WhySection.tsx
+++ pages/WhySection.tsx
@@ -2,30 +2,7 @@ import Image from "next/image"
 import PlainButton from "../components/PlainButton"
 import { motion } from "framer-motion"
 
-const whyVariants = {
-  numbers: {
-    hide: { opacity: 0, y: 150 },
-    show: { opacity: 1, y: 0, transition: { duration: 1.75 } },
-  },
-  illustration: {
-    hide: { opacity: 0, y: -150, scale: 0 },
-    show: {
-      opacity: 1,
-      y: 0,
-      scale: 1,
-      transition: { duration: 1.75, delay: 0.15 },
-    },
-  },
-  head: {
-    hide: { opacity: 0, x: -150, scale: 0 },
-    show: {
-      opacity: 1,
-      x: 0,
-      scale: 1,
-      transition: { duration: 1.75, delay: 0.25 },
-    },
-  },
-}
+import { whyVariants } from "../variants/whyVariants"
 
 const WhySection = () => {
   return (
