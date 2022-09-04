72027df1556da69c9b58c46cc1c82946c3a3e92d
headbwoi <tochukwujohn24@gmail.com>
2022-09-04 14:56:34 +0100

    added icon one

--- pages/FeatureSection.tsx
+++ pages/FeatureSection.tsx
@@ -1,5 +1,24 @@
+import Image from "next/image"
 const FeatureSection = () => {
-  return <section className="bg-primary w-full min-h-[865px]"></section>
+  return (
+    <section className="bg-primary w-full min-h-[865px] py-[100px]">
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 space-x-[99px] ">
+          <div className="w-[334px] h-[92px] flex items-center space-x-6">
+            <div className="w-20 h-20 rounded-full bg-white/10 grid place-items-center">
+              <div className="w-8 h-8 relative">
+                <Image
+                  src={"/assets/icon-barChat.svg"}
+                  alt="currency exchanged"
+                  layout="fill"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default FeatureSection
