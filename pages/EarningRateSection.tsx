e2449f9773e63141b635105ca9aa99dda4323774
headbwoi <tochukwujohn24@gmail.com>
2022-09-05 05:46:21 +0100

    created export file in pages folder export all pages from export file created earnngrate page added extra meta tags to head tag in pages./index.tsx

---
+++ pages/EarningRateSection.tsx
@@ -0,0 +1,12 @@
+
+type Props = {}
+
+const EarningRateSection = (props: Props) => {
+  return (
+    <section>
+        
+    </section>
+  )
+}
+
+export default EarningRateSection
---
+++ pages/export.ts
@@ -0,0 +1,3 @@
+export { default as HeroSection } from "./HeroSection"
+export { default as WhySection } from "./WhySection"
+export { default as EarningRateSection } from "./EarningRateSection"
