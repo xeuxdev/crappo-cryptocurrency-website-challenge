1b2f5d7b9ec0c38736668b9f7031e28f7b6393b7
headbwoi <tochukwujohn24@gmail.com>
2022-09-05 11:07:01 +0100

    added export statemnent

---
+++ pages/CryptoCurrencyList.tsx
@@ -0,0 +1,7 @@
+type Props = {}
+
+const CryptoCurrencyList = (props: Props) => {
+  return <section></section>
+}
+
+export default CryptoCurrencyList
--- pages/export.ts
+++ pages/export.ts
@@ -1,3 +1,4 @@
 export { default as HeroSection } from "./HeroSection"
 export { default as WhySection } from "./WhySection"
 export { default as EarningRateSection } from "./EarningRateSection"
+export { default as CryptoCurrencyList } from "./CryptoCurrencyList"
