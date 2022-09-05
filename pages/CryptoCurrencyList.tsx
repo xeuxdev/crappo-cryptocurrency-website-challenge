ebd30370df9e2d853f90b34d2d44e958b809cfc6
headbwoi <tochukwujohn24@gmail.com>
2022-09-05 22:10:07 +0100

    added state to manage button change

--- pages/CryptoCurrencyList.tsx
+++ pages/CryptoCurrencyList.tsx
@@ -3,7 +3,7 @@ import CryptoCard from "../components/CryptoCard"
 
 const CryptoCurrencyList = () => {
   return (
-    <section className="min-h-[617px] container px-5 md:px-7 xl:px-0">
+    <section className="min-h-[617px] container px-5 md:px-7 xl:px-0 mb-[100px]">
       <div className="heading text-center max-w-[741px] mx-auto mb-16">
         <h4 className="text-h3 sm:text-h2 text-primary ">
           Trade securely and market the high growth cryptocurrencies.
@@ -17,6 +17,18 @@ const CryptoCurrencyList = () => {
           name="Bitcoin"
           aka="BTC"
           description="Digital currency in which a record of transactions is maintained."
+        />
+        <CryptoCard
+          image="ethereum"
+          name="ethereum"
+          aka="ETH"
+          description="Blockchain technology to create and run decentralized digital applications."
+        />
+        <CryptoCard
+          image="litecoin"
+          name="litecoin"
+          aka="LTC"
+          description="Cryptocurrency that enables instant payments to anyone in the world."
         />
       </div>
     </section>
