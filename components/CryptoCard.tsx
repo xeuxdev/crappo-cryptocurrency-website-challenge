d2eb27e61a502e2713c2cb1eb1eee3604d692ed4
headbwoi <tochukwujohn24@gmail.com>
2022-09-06 11:58:09 +0100

    added settimeout function to cryptocard component

--- components/CryptoCard.tsx
+++ components/CryptoCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import SvgButton from "./SvgButton"
 type Props = {
   image: string
@@ -10,9 +10,17 @@ type Props = {
 
 const CryptoCard = (props: Props) => {
   const [svg, setSvg] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSvg(false)
+    }, 4000)
+  }, [svg])
   return (
     <div
-      className="flex items-center flex-col w-full sm:w-[370px] h-[433px] rounded-2xl hover:bg-secondary px-6 py-12 duration-500 cursor-pointer group"
+      className={`flex items-center flex-col w-full sm:w-[370px] h-[433px] rounded-2xl hover:bg-secondary px-6 py-12 duration-500 cursor-pointer group ${
+        svg ? "bg-secondary" : ""
+      }`}
       onClick={() => setSvg(!svg)}
     >
       {/* icon */}
@@ -25,14 +33,26 @@ const CryptoCard = (props: Props) => {
       </div>
       {/* tag */}
       <div className="w-[160px] h-12 space-x-2 flex items-center mb-4">
-        <p className="text-h3 text-primary group-hover:text-white capitalize">
+        <p
+          className={`text-h3 text-primary group-hover:text-white capitalize ${
+            svg ? "text-white" : ""
+          }`}
+        >
           {props.name}
         </p>
-        <p className="text-body_st_md text-gray_4 group-hover:text-gray_3 uppercase">
+        <p
+          className={`"text-body_st_md text-gray_4 group-hover:text-gray_3 uppercase ${
+            svg ? "text-gray_3" : ""
+          }`}
+        >
           {props.aka}
         </p>
       </div>
-      <p className="text-body_sm_rg text-center text-gray_3 group-hover:text-gray_4 mb-6">
+      <p
+        className={`text-body_sm_rg text-center text-gray_3 group-hover:text-gray_4 mb-6 ${
+          svg ? "text-gray_4" : ""
+        }`}
+      >
         {props.description}
       </p>
       {/*  */}
