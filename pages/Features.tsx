e87d994ebcd112496fc9c3f2eec4c6940435d0ac
headbwoi <tochukwujohn24@gmail.com>
2022-09-07 03:55:10 +0100

    added animation to features in Features.tsx

--- pages/Features.tsx
+++ pages/Features.tsx
@@ -19,45 +19,70 @@ const Features = () => {
       {/* feature 1 */}
       <div className="container min-h-[427px] flex flex-col lg:flex-row gap-[100px] mb-[120px] px-5 md:px-7 xl:px-0">
         {/* text */}
-        <div className="w-full sm:w-[454px] h-[219px] text-center lg:text-left lg:pt-6">
+        <motion.div
+          className="w-full sm:w-[454px] h-[219px] text-center lg:text-left lg:pt-6"
+          initial={featuresVariant.left.hide}
+          whileInView={featuresVariant.left.show}
+          viewport={{ once: true }}
+        >
           <p className="text-h3 text-white mb-6">Invest Smart</p>
           <p className="text-body_sm_rg text-gray_5 mb-8 sm:w-[408px]">
             Get full statistic information about the behaviour of buyers and
             sellers will help you to make the decision.
           </p>
           <PlainButton name="Learn More" />
-        </div>
+        </motion.div>
         {/* illustration */}
-        <div className="w-full h-[400px] sm:w-[580px] sm:h-[427px] relative">
+        <motion.div
+          className="w-full h-[400px] sm:w-[580px] sm:h-[427px] relative"
+          initial={featuresVariant.fade.hide}
+          whileInView={featuresVariant.fade.show}
+          viewport={{ once: true }}
+        >
           <Image src={"/assets/feature-1-img.png"} alt="chart" layout="fill" />
-        </div>
+        </motion.div>
       </div>
 
       {/* feature-2 */}
       <div className="min-h-[453px] flex items-center flex-col gap-[89px] lg:flex-row-reverse mb-[120px] px-5 md:px-7 xl:px-0">
         {/* text */}
-        <div className="w-full sm:w-[456px] h-[247px] text-center lg:text-left">
+        <motion.div
+          className="w-full sm:w-[456px] h-[247px] text-center lg:text-left"
+          initial={featuresVariant.right.hide}
+          whileInView={featuresVariant.right.show}
+          viewport={{ once: true }}
+        >
           <p className="text-h3 text-white mb-6">Detailed Statistics</p>
           <p className="text-body_sm_rg text-gray_5 mb-8 sm:w-[408px]">
             View all mining related information in realtime, at any point at any
             location and decide which polls you want to mine in.
           </p>
           <PlainButton name="Learn More" />
-        </div>
+        </motion.div>
         {/* illustraation */}
-        <div className="w-full md:w-[710px] h-[300px] md:h-[453px] relative">
+        <motion.div
+          className="w-full md:w-[710px] h-[300px] md:h-[453px] relative"
+          initial={featuresVariant.fade.hide}
+          whileInView={featuresVariant.fade.show}
+          viewport={{ once: true }}
+        >
           <Image
             src={"/assets/feature-2-img.png"}
             alt="Detailed statistics"
             layout="fill"
           />
-        </div>
+        </motion.div>
       </div>
 
       {/* feature-3 */}
       <div className="container min-h-[334px] flex flex-col lg:flex-row gap-[100px] lg:gap-16 relative z-20">
         {/* text */}
-        <div className="w-full sm:w-[454px] h-[295px] text-center lg:text-left lg:pt-6 px-5 md:px-7 xl:px-0">
+        <motion.div
+          className="w-full sm:w-[454px] h-[295px] text-center lg:text-left lg:pt-6 px-5 md:px-7 xl:px-0"
+          initial={featuresVariant.left.hide}
+          whileInView={featuresVariant.left.show}
+          viewport={{ once: true }}
+        >
           <p className="text-h3 text-white mb-6">
             Grow your profit and track your investments
           </p>
@@ -66,11 +91,16 @@ const Features = () => {
             track current and historical profit investments.
           </p>
           <PlainButton name="Learn More" />
-        </div>
+        </motion.div>
         {/* illustration */}
-        <div className="w-full h-[250px] md:w-[682px] md:h-[334px] relative">
+        <motion.div
+          className="w-full h-[250px] md:w-[682px] md:h-[334px] relative"
+          initial={featuresVariant.fade.hide}
+          whileInView={featuresVariant.fade.show}
+          viewport={{ once: true }}
+        >
           <Image src={"/assets/feature-3-img.png"} alt="chart" layout="fill" />
-        </div>
+        </motion.div>
       </div>
     </section>
   )
