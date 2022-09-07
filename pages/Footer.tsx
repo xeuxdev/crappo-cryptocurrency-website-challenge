46b1a7cd362a773d50248a65512fb5f564158da4
headbwoi <tochukwujohn24@gmail.com>
2022-09-07 06:57:35 +0100

    added init styles to footer

--- pages/Footer.tsx
+++ pages/Footer.tsx
@@ -1,5 +1,7 @@
 const Footer = () => {
-  return <section></section>
+  return (
+    <section className="bg-primary h-[552px] w-full px-5 md:px-7 xl:px-0 "></section>
+  )
 }
 
 export default Footer
